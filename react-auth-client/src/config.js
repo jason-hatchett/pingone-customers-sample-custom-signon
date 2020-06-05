04d3895e40c3e602362c9037bf14175cd68ebf4b
Jason Hatchett <jason@hatchett.net>
2020-06-04 20:45:23 -0500

    Update config.js

--- react-auth-client/src/config.js
+++ react-auth-client/src/config.js
@@ -13,9 +13,9 @@ export default {
   // Specifies the grant type of the token request. Options are authorization_code, implicit, and client_credentials
   grantType: "implicit",
   // Specifies the URL that specifies the return entry point of the application. This is a required property.
-  redirectUri: process.env.DEPLOY_URL,
+  redirectUri: process.env.REACT_APP_URL,
   // Specifies an optional parameter that specifies the URL to which the browser is redirected after a logout has been performed.
-  logoutRedirectUri: process.env.DEPLOY_URL,
+  logoutRedirectUri: process.env.REACT_APP_URL,
   // Specifies permissions that determine the resources that the application can access. This parameter is not required, but it is needed to specify accessible resources.
   scope: "profile address email phone",
   // Specifies whether the user is prompted to login for re-authentication. The prompt parameter can be used as a way to check for existing authentication, verifying that the user is still present for the current session. For prompt=none, the user is never prompted to login to re-authenticate, which can result in an error if authentication is required. For prompt=login, if time since last login is greater than the max-age, then the current session is stashed away in the flow state and treated in the flow as if there was no previous existing session. When the flow completes, if the flow’s user is the same as the user from the stashed away session, the stashed away session is updated with the new flow data and persisted (preserving the existing session ID). If the flow’s user is not the same as the user from the stashed away session, the stashed away session is deleted (logout) and the new session is persisted.
